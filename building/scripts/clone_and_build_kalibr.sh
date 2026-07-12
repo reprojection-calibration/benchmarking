0ab2828c7926b012b5073a5dc461e2b32152c24c
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-12 16:16:56 +0200

    remove git install and add build step

--- building/scripts/clone_and_build_kalibr.sh
+++ building/scripts/clone_and_build_kalibr.sh
@@ -2,14 +2,12 @@
 
 set -eoux pipefail
 
+# WARN(Jack): This script requires that you have git and docker installed! The best way to manage that at this time
+# is not clear!
+
 # TODO(Jack): Make this a default value but let the user set it via an env var!
 script_folder="$(dirname "$(realpath -s "$0")")"
 THIRDPARTY_DIR="${script_folder}/../../thirdparty"
-
-apt-get update
-apt-get install --no-install-recommends --yes \
-    git
-rm --force --recursive /var/lib/apt/lists/*
 
 # NOTE(Jack): We need to solve the case where we are running this locally on a users system that needs sudo to
 # update/install apt packages, but we do not want the git clone to happen as the super user because that causes
@@ -20,3 +18,8 @@ if [[ -n "${SUDO_USER:-}" ]]; then
 else
     git clone "https://github.com/ethz-asl/kalibr.git" "${THIRDPARTY_DIR}/kalibr"
 fi
+
+docker build \
+    --file "${THIRDPARTY_DIR}/kalibr/Dockerfile_ros1_20_04" \
+    --tag kalibr \
+    "${THIRDPARTY_DIR}/kalibr"
