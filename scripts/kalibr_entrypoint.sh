d50c3be152d0fcc1d3111d401b411ac3ff140b16
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-13 17:50:31 +0200

    tidy entrypoint

--- scripts/kalibr_entrypoint.sh
+++ scripts/kalibr_entrypoint.sh
@@ -4,9 +4,7 @@ set -eoux pipefail
 
 # NOTE(Jack): These three lines are the classic lines found in the Kalibr dockerfile entrypoint
 export KALIBR_MANUAL_FOCAL_LENGTH_INIT=1
-set +u
-source "${WORKSPACE}/devel/setup.bash"
-set -u
+set +u; source "${WORKSPACE}/devel/setup.bash"; set -u
 cd "${WORKSPACE}"
 
 # TODO(Jack): Updating and installing each to time we run the script is not so clean but I don't want to go change
@@ -16,10 +14,11 @@ apt-get install --no-install-recommends --yes \
     jq
 rm --force --recursive /var/lib/apt/lists/*
 
+# TODO(Jack): Hardcoding these paths here does not seem like an eloquent solution but it is quick, easy, and gets the
+# job done!
 dataset_specification_json="/temporary/config/dataset_specification.json"
-target_config="/temporary/config/kalibr/april_6x6_80x80cm.yaml"
-
 [[ -f "${dataset_specification_json}" ]] || { echo "Error: json dataset specification does not exist: ${dataset_specification_json}" >&2; exit 1; }
+target_config="/temporary/config/kalibr/april_6x6_80x80cm.yaml"
 [[ -f "${target_config}" ]] || { echo "Error: Kalibr target configuration does not exist: ${target_config}" >&2; exit 1; }
 
 # NOTE(Jack): Iteration logic adopted from https://stackoverflow.com/questions/68121082/how-to-iterate-over-json-array-with-jq
@@ -37,16 +36,11 @@ while read bag_i; do
           --target "${target_config}" \
           --topics "${camera_i}"
 
-        # NOTE(Jack): We need to clean the camera name from having slashes (ex. like in a ROS topic) because otherwise the
-        # mkdir command below could interpret that as a multilevel path.
+        # NOTE(Jack): We need to clean the camera name from having slashes (ex. like in a ROS topic) because otherwise
+        # the mkdir command below will interpret that as a multilevel path.
         camera_name="${camera_i#/}"
         camera_name="${camera_name//\//_}"
 
-        # TODO(Jack): We should protect against the case where there are no output files to move, if that is an error condition
-        # that we realistically need to worry about.
-        # TODO(Jack): Define a variable for the data path?
-        # TODO(Jack): It might be a smart idea to be more specific about which files we move, for example all files that do not
-        # end in .bag or all files that match the kalibr output format pattern, etc.
         # NOTE(Jack): Kalibr does not let us specify an output path or filename for the output diagnostics. Therefore
         # we need to manually move all the outputs into a batch specific (i.e. camera name and model) directory.
         mkdir --parents "/data/kalibr/${camera_name}"
