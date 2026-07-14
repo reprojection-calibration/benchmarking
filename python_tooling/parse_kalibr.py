8dfd223a609672f397831f24984aa172b4d44066
reprojection-calibration <reprojection.calibration@gmail.com>
2026-07-14 06:49:58 +0200

    test_load_camchain

--- python_tooling/parse_kalibr.py
+++ python_tooling/parse_kalibr.py
@@ -19,6 +19,8 @@ def collect_camchain_files(kalibr_directory):
 
 
 def load_camchain(path):
+    path = Path(path)
+
     with path.open("r", encoding="utf-8") as file:
         data = yaml.safe_load(file)
 
--- python_tooling/test_parse_kalibr.py
+++ python_tooling/test_parse_kalibr.py
@@ -1,6 +1,6 @@
 import tempfile
 from pathlib import Path
-from tempfile import TemporaryDirectory
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 from unittest import TestCase
 
 from parse_kalibr import (CAMCHAIN_SUFFIX, collect_camchain_files,
@@ -9,6 +9,9 @@ from parse_kalibr import (CAMCHAIN_SUFFIX, collect_camchain_files,
 
 class TestParseKalibr(TestCase):
     def test_collect_camchain_files(self):
+        # From - https://adamj.eu/tech/2024/12/30/python-temporary-files-directories-unittest/ - "enterContext() enters
+        # the NamedTemporaryFile context manager and runs its exit method at the end of the test. Using enterContext()
+        # avoids indenting the whole test, as would be required using with."
         temp_dir = self.enterContext(TemporaryDirectory())
         for camera in ("cam0_image_raw", "cam1_image_raw"):
             camera_dir = Path(temp_dir) / camera
@@ -25,3 +28,17 @@ class TestParseKalibr(TestCase):
                 Path(f"{temp_dir}/cam1_image_raw/foo-bar-camchain.yaml"),
             ],
         )
+
+    def test_load_camchain(self):
+        temp_file = self.enterContext(NamedTemporaryFile(mode="w+", suffix=".yaml"))
+        temp_file.write(
+            """
+            foo:
+              bar: 111
+            """
+        )
+        temp_file.flush()
+
+        result = load_camchain(temp_file.name)
+
+        self.assertEqual(({"foo": {"bar": 111}}, Path(temp_file.name)), result)
