3738c99817f988ad4787c867254200a663a54099
reprojection-calibration <reprojection.calibration@gmail.com>
2026-08-14 07:19:40 +0200

    create camera info table

--- python_tooling/process_reprojection_dbs.py
+++ python_tooling/process_reprojection_dbs.py
@@ -78,6 +78,26 @@ def update_database(db_path, rows):
                 ),
             )
 
+            # TODO(Jack): Can we use the sql directly from the third party reprojection directory?
+            db.execute(
+                """
+                CREATE TABLE IF NOT EXISTS camera_info
+                (
+                    step_id      INTEGER NOT NULL,
+                    asset_id     INTEGER NOT NULL,
+                    camera_model TEXT    NOT NULL CHECK ( camera_model IN
+                                                          ('double_sphere', 'pinhole', 'pinhole_radtan4', 'unified_camera_model')),
+                    height       INTEGER NOT NULL,
+                    width        INTEGER NOT NULL,
+
+                    FOREIGN KEY (step_id) REFERENCES steps (id) ON DELETE CASCADE,
+                    FOREIGN KEY (asset_id) REFERENCES assets (id) ON DELETE CASCADE,
+                    PRIMARY KEY (step_id, asset_id)
+                );
+                """,
+                ()
+            )
+
             db.execute(
                 """
                 INSERT INTO camera_info (step_id,
